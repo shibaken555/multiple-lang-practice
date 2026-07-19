51830e3e79ad5e23bc18098ecb15eb59f48f5588
shibaken <ychaishan@gmail.com>
2026-07-20 01:24:10 +0900

    適当に練習したもの

--- ts/practice1.js
+++ ts/practice1.js
@@ -1,2 +1,14 @@
 "use strict";
 console.log("Hey What's Up");
+console.log("文字列中のダブルクォーテーション(\")は注意が必要");
+let productPrice = 1200;
+const CONSUMPTION_TAX_RATE = 1.1;
+let totalAmount = productPrice * CONSUMPTION_TAX_RATE;
+console.log(totalAmount);
+function calculatetotalAmount(productPrice, taxRate) {
+    let totalAmount = productPrice * taxRate;
+    return totalAmount;
+}
+console.log("商品価格は" + "1000円です");
+console.log("消費税率は" + "10%です");
+console.log(calculatetotalAmount(1000, 1.1));
--- ts/practice1.ts
+++ ts/practice1.ts
@@ -1,1 +1,16 @@
 console.log("Hey What's Up");
+console.log("文字列中のダブルクォーテーション(\")は注意が必要");
+
+let productPrice:number = 1200;
+const CONSUMPTION_TAX_RATE = 1.1;
+let totalAmount:number = productPrice * CONSUMPTION_TAX_RATE;
+console.log(totalAmount);
+
+function calculatetotalAmount(productPrice:number,taxRate:number){
+   let totalAmount:number = productPrice * taxRate;
+   return totalAmount;
+}
+
+console.log("商品価格は" + "1000円です");
+console.log("消費税率は" + "10%です");
+console.log(calculatetotalAmount(1000,1.1));
